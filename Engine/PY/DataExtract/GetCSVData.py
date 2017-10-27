1a416242011fc60f3c4df5df919e6d5b8fe11d5c
Aki <damien.bertet@gmail.com>
2017-10-27 18:23:27 +0200

    csv extract

--- Engine/PY/DataExtract/GetCSVData.py
+++ Engine/PY/DataExtract/GetCSVData.py
@@ -1,9 +1,13 @@
 import csv
 
-def readCSVFile(p_csv_full_name):
+def readCSVFile(p_csv_full_name, p_print_html):
     with open(p_csv_full_name, 'r') as f:
         reader = csv.reader(f)
         result_list = list(reader)
+        if(p_print_html):
+            print(result_list)
+            for(row in result_list):
+                print(row[1])
 
 
-readCSVFile('C:\Dev\Git\WeData\Data_Samples\CSV\sales.csv')
+readCSVFile('C:\\Dev\\Git\\WeData\\Data_Samples\\CSV\\sales.csv', True)
