000ae0aa0b2c18b8dbdd03d9a6a3343c9886e364
Aki <damien.bertet@gmail.com>
2017-11-02 17:34:44 +0100

    bla

--- Engine/PY/DataExtract/GetCSVData.py
+++ Engine/PY/DataExtract/GetCSVData.py
@@ -1,13 +1,23 @@
 import csv
+import os
 
 def readCSVFile(p_csv_full_name, p_print_html):
     with open(p_csv_full_name, 'r') as f:
-        reader = csv.reader(f)
+        reader = csv.reader(f, delimiter=';')
         result_list = list(reader)
         if(p_print_html):
-            print(result_list)
-            for row in result_list:
-                print('ligne : ' + row)
+            #print(result_list)
+            print('<table>')
+            print('<tr><th>' + '</th><th>'.join(result_list[0]) + '</th></tr>')
+            for idx in range(1, len(result_list)):
+                #print('ligne : ' + row[1])
+                print('<tr><td>' + '</td><td>'.join(result_list[idx]) + '</td></tr>')
+            
+            print('</table>')
 
+
+clear = lambda: os.system('cls')
+clear()
 
 readCSVFile('C:\\Dev\\Git\\WeData\\Data_Samples\\CSV\\sales.csv', True)
+#readCSVFile('D:\\Perso\\DEV\\WeData\\Data_Samples\CSV\\sales.csv', True)
