782f1b0cc5a1f1293e0f8225eb55bec4e567f330
Aki <damien.bertet@gmail.com>
2018-01-25 18:02:43 +0100

    translation

--- Engine/PY/DataExtract/HappyOrNot.py
+++ Engine/PY/DataExtract/HappyOrNot.py
@@ -4,11 +4,13 @@ from textblob import TextBlob
 def analize_sentiment(text):
     analysis = TextBlob(text)
     print("language : " + analysis.detect_language())
+    trad = analysis.translate(from_lang="auto", to="en")
+    print("en : " + str(trad))
     #French Version
     #analysis = TextBlob(text, pos_tagger=PatternTagger(), analyzer=PatternAnalyzer())
-    if analysis.sentiment.polarity > 0:
+    if trad.sentiment.polarity > 0:
         return 1
-    elif analysis.sentiment.polarity == 0:
+    elif trad.sentiment.polarity == 0:
         return 0
     else:
         return -1
