deed092638d7002a078582467b1f220504ca7b56
Aki013 <damien.bertet@gmail.com>
2018-01-30 22:56:44 +0100

    include libs

--- Engine/PY/DataExtract/tweets_steam.py
+++ Engine/PY/DataExtract/tweets_steam.py
@@ -1,11 +1,11 @@
-import libs.tweepy.utils
+import tweepy
 import json
-
+from textblob import TextBlob
 
 
 #Read Config_File
-with open('C:\DEV\Python\config.json') as json_data_file:
-#with open('D:\Perso\DEV\Python\config.json') as json_data_file:
+#with open('C:\DEV\Python\config.json') as json_data_file:
+with open('D:\Perso\DEV\Python\config.json') as json_data_file:
     config_data = json.load(json_data_file)
 
 consumer_key=config_data['twitter']['CONSUMER_KEY']
@@ -13,10 +13,21 @@ consumer_secret=config_data['twitter']['CONSUMER_SECRET']
 access_token=config_data['twitter']['ACCESS_TOKEN']
 access_token_secret=config_data['twitter']['ACCESS_SECRET']
 
+def analize_sentiment(text):
+    analysis = TextBlob(text)
+    #print("language : " + analysis.detect_language())
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+
 
 # This listener will print out all Tweets it receives
 class PrintListener(tweepy.StreamListener):
     nb_tweets = 0
+    sum_pol = 0
     def on_data(self, data):
         # Decode the JSON data
         tweet = json.loads(data)        
@@ -26,7 +37,9 @@ class PrintListener(tweepy.StreamListener):
         tweet_text = str(tweet['text'].encode('ascii', 'ignore'))
         # Print out the Tweet
         PrintListener.nb_tweets+=1
+        PrintListener.sum_pol += analize_sentiment(tweet_text)        
         print(str(PrintListener.nb_tweets) + ' - ' + tweet_date + ' - @' + tweet_author + ' :   ' + tweet_text)
+        print('     sum pol : ' + str(PrintListener.sum_pol))
         #print('@%s: %s' % (tweet['user']['screen_name'], tweet['text'].encode('ascii', 'ignore')))
 
     def on_error(self, status):
@@ -36,8 +49,9 @@ class PrintListener(tweepy.StreamListener):
 if __name__ == '__main__':
     listener = PrintListener()
 
+    Words = 'Death'
     # Show system message
-    print('I will now print Tweets containing "Python"! ==>')
+    print('I will now print Tweets containing "' + Words + '"! ==>')
 
     # Authenticate
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -45,4 +59,4 @@ if __name__ == '__main__':
 
     # Connect the stream to our listener
     stream = tweepy.Stream(auth, listener)
-    stream.filter(track=['Bitcoin'])
+    stream.filter(track=[Words])
